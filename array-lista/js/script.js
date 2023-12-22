553ed347b85420e23b89dfe0e3249fb3cb5d74ae
Francesco <francesco.pasquinoni@hotmail.it>
2023-12-22 17:38:44 +0100

    Snack 1: TuttiFrutti corretto

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,20 +1,30 @@
 console.log("JS OK")
 
 const frigorifero = [ "banana", "mela", "pera", "ciliegia", "arancia", "mandarino", "cocomero", "fragola"];
-
-console.log(frigorifero);
+let listaDellaSpesa = [];
+const cocomero = "cocomero";
 
 frigorifero.push("pesca");
 
-console.log(frigorifero);
+console.table(frigorifero);
 
-const cocomero = "cocomero"
+console.table(listaDellaSpesa);
 
 for (let i=0 ; i < frigorifero.length ; i++){
-    if (cocomero === frigorifero[i]){
-        console.log("Trovato! Devo solo preparare il cocktail...", "il cocomero era proprio qui:", i);
-    } else {
-        "Oh no, devo uscire a comprare il cocomero!"
+    if (cocomero !== frigorifero[i] && listaDellaSpesa.length === 0){
+        listaDellaSpesa.push(cocomero);
     }
-    
 }
+for (let i=0 ; i < frigorifero.length ; i++){
+    if (cocomero === frigorifero[i]) {
+    console.log("Trovato! Devo solo preparare il cocktail...");
+    listaDellaSpesa = []
+    }
+} 
+
+if (listaDellaSpesa.length !== 0 ) {
+    console.log("Oh no, devo uscire a comprare il cocomero!");
+}
+
+console.log("cosa ho in lista quindi?", listaDellaSpesa)
+
